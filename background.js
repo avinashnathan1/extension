d03f148a37454ad15fcc9b04a690400ead5dc32b
Arjun Bahel <abahel2@illinois.edu>
2020-03-18 19:01:17 -0500

    Timer stuff

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
@@ -12,9 +14,19 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
       });
   });
+
+let timerID;
+let timerTime;
   
-
-
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.cmd === 'START_TIMER') {
+      timerTime = new Date(request.when);
+      timerID = setTimeout(() => {}, timerTime.getTime() - Date.now());
+    } else if (request.cmd === 'GET_TIME') {
+      sendResponse({ time: timerTime });
+    }
+  });
+  
 
   
   /**
@@ -22,11 +34,22 @@ chrome.runtime.onInstalled.addListener(function() {
    * Submit the page
    * Program the timer 
    * 
+   * If they have opened the browser within the email period, try to open up the 
+   * pop-up itself to remind. (They do not have to rely on checking their email)
+   * 
+   * Check to see if it was a montly, weekly, or less than a week trial and
+   * come up with emailing system accordingly. 
+   * 
+   * For length of subscription consider using 3 different input boxes that handles 
+   * the month, days, and hours. 
+   * 
    */
 
   /**
-   * 
+   * Intergrate Timer to background.js:
    * https://stackoverflow.com/questions/58132891/chrome-timer-extension-how-to-keep-timer-running-even-when-extension-is-closed
+   * Coding the timer:
+   * https://www.w3schools.com/howto/howto_js_countdown.asp 
    * 
    * 
    * 
